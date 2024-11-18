47d07b3ff3e01555f03d27f66d21b575a89e13a8
AseelN <aseelalnefaie03@gmail.com>
2024-11-18 18:14:51 +0300

    first commit

--- src/group1_project/Group1_project.java
+++ src/group1_project/Group1_project.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package group1_project;
 
-import java.util.*;
 public class Group1_project {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s1 = in.next();
-        System.out.println(s1);
+        System.out.println("hello");
         
     }
     
