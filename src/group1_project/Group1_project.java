c8c93821398b27fb3cdca3d5a992011850271493
AseelN <aseelalnefaie03@gmail.com>
2024-11-19 21:18:43 +0300

    done

--- src/group1_project/Group1_project.java
+++ src/group1_project/Group1_project.java
@@ -3,7 +3,7 @@ package group1_project;
 public class Group1_project {
 
     public static void main(String[] args) {
-        System.out.println("Hello Aseel done");
+        System.out.println("Hello Aseel done finaaaaaal");
 
 
     }
