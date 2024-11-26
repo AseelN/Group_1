4ad8bdd887d2c0125ce0846b03d589f19ba6db6d
AseelN <aseelalnefaie03@gmail.com>
2024-11-26 22:00:35 +0300

    first commit

--- src/group1_project/Group1_project.java
+++ src/group1_project/Group1_project.java
@@ -3,18 +3,7 @@ package group1_project;
 public class Group1_project {
 
     public static void main(String[] args) {
-        System.out.println("Hello Aseel done finaaaaaal");
-        System.out.println("Layan");
-        System.out.println("raya");
-        System.out.println("test");
-        System.out.println("Hello Aseel done finaaaaaal");
-        System.out.println("Layan");
-        System.out.println("raya");
-        System.out.println("test");
         
-        
-        
-        System.out.println("f");
         
         
 
