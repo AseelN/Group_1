666650637043a76aaf004e39fddcacf99595856f
RayaAlyoubi <ralyoubi0084@stu.kau.edu.sa>
2024-11-27 22:45:42 +0300

    k

--- src/group1_project/Group1_project.java
+++ src/group1_project/Group1_project.java
@@ -2,11 +2,44 @@ package group1_project;
 
 public class Group1_project {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         
+        System.out.println("Choose category to help search for information:");
+        String[] categories = { 
+            "sahl system", 
+            "desktop interface", 
+            "security interface", 
+            "ip address connfigurtion", 
+            "change password" 
+        };
         
+        for(int i = 0; i < categories.length; i ++){
+            System.out.println(categories[i]);
+        }
+        
+        String str = in.next();
+        String what = validation(categories, str);
+        
+    }
+    
+    public static String validation(String [] cat,String str){
         
 
+        
+        for(int i = 0; i < cat.length ; i++){
+            if(cat[i].equalsIgnoreCase(str))
+                t = i;
+           
+        }
+        
+        if(t != -1)
+            return "What can we help you with?";
+        else
+            return "Not found, try again.";
     }
+    
+    
+}
 
 }
